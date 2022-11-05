ad6471c4e16db9821344cf8fde6d7c5a02c4e0ee
SeymaDemir <seymademir35@gmail.com>
2022-11-05 20:10:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(arr){
+    const answer = arr.find(({result}) => result === 'W')
+    if(answer){
+        return answer.year;
+    }
+    else{
+        return undefined
+    }
+}
